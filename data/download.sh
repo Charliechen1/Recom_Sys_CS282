2505e3535a369364d87d18fcc5cccd3b47f52daa
Charliechen1 <Charliechen_1@163.com>
2020-04-24 14:59:52 -0700

    [Charlie 24042020] Added comment on how to use download.sh

--- data/download.sh
+++ data/download.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 # script to download Amazon review data
-# $1 = Gift_Cards for gift_card domain data
+# e.g. $1 = Gift_Cards for gift_card domain data
+#
+# for more details please refer to http://deepyeti.ucsd.edu/jianmo/amazon/index.html
+# right click on the hyper link, choose copy url to see the name of the dataset
+# Generally speaking, replace the space with underscore works.
+# e.g. Gift Cards -> Gift_Cards
+
 echo "downloading $1 data"
 wget http://deepyeti.ucsd.edu/jianmo/amazon/categoryFiles/$1.json.gz
+
+echo "downloading $1 meta"
+wget http://deepyeti.ucsd.edu/jianmo/amazon/metaFiles/$1.json.gz
